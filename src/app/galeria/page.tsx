d41006fd63daf013af5f44b08f2fa76c63932ba1
pedrol-9 <psanabria999@gmail.com>
2026-05-07 15:41:07 -0500

    Art Showcase implemented to show off the art of Santiago from Instagram

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -4,15 +4,17 @@ import React from "react";
 import { Navbar } from "@/layout/Navbar";
 import { Footer } from "@/layout/Footer";
 import { CardCarousel } from "./components/CardCarousel";
+import { ArtShowcase } from "./components/ArtShowcase";
 
 export default function GaleriaPage() {
   return (
     <>
       <Navbar />
       <main className="relative min-h-screen font-sans overflow-x-hidden selection:bg-[#C1533B] selection:text-[#100F0D] flex flex-col" style={{ background: "#100F0D", color: "#DDD8CF" }}>
-        <div className="flex-1 w-full py-20 flex flex-col">
+        <div className="w-full pt-20 flex flex-col">
           <CardCarousel />
         </div>
+        <ArtShowcase />
         <Footer />
       </main>
     </>
